742d0139508f0dc1f5ef9a6cc70c389535e885bd
Pedro Moraes <pedrowebedesign@gmail.com>
2023-06-21 16:50:57 -0300

    Build: Base do projeto
    Código, anotações e estrutura do projeto

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+//The default export of _app.js is a top-level React component that wraps all the pages in your application. You can use this component to keep state when navigating between pages, or to add global styles as we're doing here. 
+//https://nextjs.org/docs/advanced-features/custom-app
+
+import '../styles/globals.css';
+
+export default function App({Component, pageProps}){
+    return <Component {...pageProps} />;
+}
